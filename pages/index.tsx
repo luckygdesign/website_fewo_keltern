c4052fc44e3c279f2ceb35989d86e0b0be3f7247
luckygro <lukas@luckyg.de>
2020-08-20 17:07:03 +0200

    IndexPage: add Ausstattungsliste (with fontawesome)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faThermometerThreeQuarters,
+  faDog,
+  faSmokingBan,
+  faWifi,
+  faCar,
+  faUsers,
+  faCalendarCheck,
+  faCreditCard,
+  faBed,
+  faShower,
+  faUtensils,
+} from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
 export default function Home() {
   return (
     <div>
@@ -65,7 +81,7 @@ export default function Home() {
                 man über Niebelsbach/ Straubenhardt/ Dobel, aber auch die
                 Goldstadt Pforzheim, die badische "Hauptstadt" Karlsruhe,
                 Etllingen, Rastatt sind nicht weit.Überquert man den Rhein, ist
-                man in der Pfalz oder im Elsaß und den Vogesen.{" "}
+                man in der Pfalz oder im Elsaß und den Vogesen. Mehr Infos:{" "}
                 <a href="https://www.keltern.de" target="_blanc">
                   www.keltern.de
                 </a>
@@ -75,8 +91,110 @@ export default function Home() {
         </div>
       </div>
       <div className="py-16 bg-gray-50 overflow-hidden">
-        <h2>Ausstattung</h2>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="relative z-10 mb-12 lg:mb-0">
+            <h3 className="text-lg leading-6 font-medium text-gray-900 py-6">
+              Ausstattung
+            </h3>
+
+            <div className="md:grid md:grid-cols-3 md:gap-6">
+              {ausstattungItems.map((a, i) => (
+                <AusstattungItem {...a} key={i} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
+type IAusstattungItem = {
+  title: string;
+  icon: IconProp;
+  details?: string[];
+};
+
+const ausstattungItems: IAusstattungItem[] = [
+  {
+    title: "Küche",
+    icon: faUtensils,
+    details: [
+      "Herd mit zwei Kochplatten, Dunstabzug und Backofen",
+      "Spülmaschine",
+      "Spülbecken",
+      "Geschirr, Töpfe und Pfannen",
+    ],
+  },
+  {
+    title: "Wohn- und Schlafbereich",
+    icon: faBed,
+    details: [
+      "Schrankwand und Kleiderschrank",
+      "Couch, verwandelbar in Bett für 1-2 Personen",
+      "mit extra Matratze 160 x 190 cm",
+      "kleiner Beistelltisch",
+      "Garderobe mit Schuhablage",
+    ],
+  },
+  {
+    title: "Bad",
+    icon: faShower,
+    details: ["barrierefreie Dusche", "Waschbecken", "Toilette"],
+  },
+  {
+    title: "Heizung",
+    icon: faThermometerThreeQuarters,
+  },
+  {
+    title: "keine Haustiere",
+    icon: faDog,
+  },
+  {
+    title: "Nichtraucher",
+    icon: faSmokingBan,
+  },
+  {
+    title: "freies WLAN",
+    icon: faWifi,
+  },
+  {
+    title: "Parkplatz an Haus",
+    icon: faCar,
+  },
+  {
+    title: "Belegung: 1-2 Personen",
+    icon: faUsers,
+  },
+  {
+    title: "Anreise / Abreise",
+    icon: faCalendarCheck,
+    details: ["Anreise ab 16:00 Uhr", "Abreise bis 11:00 Uhr"],
+  },
+  {
+    title: "Preise",
+    icon: faCreditCard,
+    details: [
+      "1. und 2. Tag: 95,00 € pro Tag",
+      "ab 3 Tagen: 90,00 € pro Tag",
+      "ab 4 Tagen: 85,00 € pro Tag",
+      "ab 5 Tagen: 80,00 € pro Tag",
+    ],
+  },
+];
+
+const AusstattungItem = (item: IAusstattungItem) => (
+  <div className="p-6 bg-gray-100 rounded-lg shadow-md">
+    <div className="flex items-center text-gray-700">
+      <FontAwesomeIcon className="mr-2 text-lg fill-current" icon={item.icon} />
+      <span className="">{item.title}</span>
+    </div>
+    {item.details && (
+      <ul className="pt-4 text-gray-500">
+        {item.details.map((a, i) => (
+          <li key={i}>{a}</li>
+        ))}
+      </ul>
+    )}
+  </div>
+);
